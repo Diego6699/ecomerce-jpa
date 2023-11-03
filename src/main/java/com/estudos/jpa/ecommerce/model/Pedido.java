9cea7a3d39b1359014a23eb4503908627a4a2e8e
Diego Leonardo <diegoleonardo9966@gmail.com>
2023-11-03 15:54:30 -0300

    Pedido estruturado

--- src/main/java/com/estudos/jpa/ecommerce/model/Pedido.java
+++ src/main/java/com/estudos/jpa/ecommerce/model/Pedido.java
@@ -1,5 +1,32 @@
 package com.estudos.jpa.ecommerce.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "pedido")
 public class Pedido {
+	@Id
+	private Integer id;
+
+	private LocalDateTime dataPedido;
+
+	private LocalDateTime dataConclusao;
+
+	private Integer notaFiscalId;
+
+	private BigDecimal total;
+
+	private StatusPedido status;
 
 }
