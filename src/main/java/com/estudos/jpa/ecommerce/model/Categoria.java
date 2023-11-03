8f8beedc32d3465490467e3fde5cb056f2d7a08b
Diego Leonardo <diegoleonardo9966@gmail.com>
2023-11-03 14:40:11 -0300

    Categoria estruturado

--- src/main/java/com/estudos/jpa/ecommerce/model/Categoria.java
+++ src/main/java/com/estudos/jpa/ecommerce/model/Categoria.java
@@ -1,6 +1,24 @@
 package com.estudos.jpa.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table
 public class Categoria {
 
+	@Id
+	@EqualsAndHashCode.Include
+	private Integer id;
+	
+	private String nome;
+	
+	private Integer categoriaPaiId;
 }
