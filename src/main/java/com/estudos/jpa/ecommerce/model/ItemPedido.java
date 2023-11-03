728d35dfee784fbb8f874fcc77bb6b04d515a463
Diego Leonardo <diegoleonardo9966@gmail.com>
2023-11-03 15:43:53 -0300

    ItemPedido estruturado

--- src/main/java/com/estudos/jpa/ecommerce/model/ItemPedido.java
+++ src/main/java/com/estudos/jpa/ecommerce/model/ItemPedido.java
@@ -1,5 +1,28 @@
 package com.estudos.jpa.ecommerce.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "item_pedido")
 public class ItemPedido {
+	
+	private Integer id;
+	
+	private Integer pedidoId;
+	
+	private Integer produtoId;
+	
+	private BigDecimal precoProduto;
+	
+	private Integer quantidade;
 
 }
