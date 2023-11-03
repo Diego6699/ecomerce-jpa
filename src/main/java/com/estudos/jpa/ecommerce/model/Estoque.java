8dbccf261bd89ba3b81f1dcdd4080d03797e9dc8
Diego Leonardo <diegoleonardo9966@gmail.com>
2023-11-03 15:40:39 -0300

    Estoque estruturado

--- src/main/java/com/estudos/jpa/ecommerce/model/Estoque.java
+++ src/main/java/com/estudos/jpa/ecommerce/model/Estoque.java
@@ -1,5 +1,24 @@
 package com.estudos.jpa.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "estoque")
 public class Estoque {
 
+	@Id
+	@EqualsAndHashCode.Include
+	private Integer id;
+
+	private Integer produtoId;
+
+	private Integer quantidade;
 }
