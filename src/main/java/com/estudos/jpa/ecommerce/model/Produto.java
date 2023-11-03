3efd5e7dce2d2c58b2862738d469ce042f4d94de
Diego Leonardo <diegoleonardo9966@gmail.com>
2023-11-03 14:37:05 -0300

    Produto estruturado

--- src/main/java/com/estudos/jpa/ecommerce/model/Produto.java
+++ src/main/java/com/estudos/jpa/ecommerce/model/Produto.java
@@ -1,5 +1,29 @@
 package com.estudos.jpa.ecommerce.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "produto")
 public class Produto {
+	
+	@Id
+	@EqualsAndHashCode.Include
+	private Integer id;
+	
+	private String nome;
+	
+	private String descricao;
+	
+	private BigDecimal preco;
 
 }
