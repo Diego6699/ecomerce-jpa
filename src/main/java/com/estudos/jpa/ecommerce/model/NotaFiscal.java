512f486e8c00e3e77523a03725f031ee255eb469
Diego Leonardo <diegoleonardo9966@gmail.com>
2023-11-03 15:46:47 -0300

    NotaFiscal estruturado

--- src/main/java/com/estudos/jpa/ecommerce/model/NotaFiscal.java
+++ src/main/java/com/estudos/jpa/ecommerce/model/NotaFiscal.java
@@ -1,5 +1,31 @@
 package com.estudos.jpa.ecommerce.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "nota_fiscal")
 public class NotaFiscal {
+	
+	@Id
+	@EqualsAndHashCode.Include
+	private Integer id;
+	
+	private Integer pedidoId;
+	
+	private Integer produtoId;
+	
+	private BigDecimal precoproduto;
+	
+	private Integer quantidade;
 
 }
